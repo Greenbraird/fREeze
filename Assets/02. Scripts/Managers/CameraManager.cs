e4d86c22f53d672fb673c0169e47fff2109e0438
Kimsijun <siwancom22@@cau.ac.kr>
2024-07-02 21:44:12 +0900

    Automated commit on 2024-07-02 21:44:08

--- Assets/02. Scripts/Managers/CameraManager.cs
+++ Assets/02. Scripts/Managers/CameraManager.cs
@@ -7,8 +7,9 @@ public class CameraManager : MonoBehaviour
     public GameObject mainCharater;
     void Update()
     {
-        Vector3 camereUpdis = mainCharater.transform.up;
-        Vector3 camereBackdis = mainCharater.transform.forward * -0.4f;
-        transform.position = camereBackdis;
+        Vector3 camereUpdis = mainCharater.transform.up*3;
+        Vector3 camereBackdis = mainCharater.transform.forward * -4f;
+        transform.position = new Vector3(mainCharater.transform.position.x, mainCharater.transform.position.y, mainCharater.transform.position.z) + camereUpdis + camereBackdis;
+
     }
 }
