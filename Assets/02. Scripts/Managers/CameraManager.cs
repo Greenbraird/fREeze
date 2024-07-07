b0fb355f0af9ca445ad872ad96fd7f83444488f4
Kimsijun <siwancom22@@cau.ac.kr>
2024-07-07 18:46:38 +0900

    Automated commit on 2024-07-07 18:46:35

--- Assets/02. Scripts/Managers/CameraManager.cs
+++ Assets/02. Scripts/Managers/CameraManager.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     public GameObject mainCharater;
-    void Update()
+
+    float fixedXRotation = 11;
+    void LateUpdate()
     {
-        Vector3 camereUpdis = mainCharater.transform.up*3;
+        Vector3 camereUpdis = mainCharater.transform.up * 3;
         Vector3 camereBackdis = mainCharater.transform.forward * -4f;
         transform.position = new Vector3(mainCharater.transform.position.x, mainCharater.transform.position.y, mainCharater.transform.position.z) + camereUpdis + camereBackdis;
 
+        // 타겟 오브젝트를 바라보는 방향으로 카메라의 회전 값을 설정
+        Vector3 direction = mainCharater.transform.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+
+        // X축 회전 값 고정
+        rotation = Quaternion.Euler(fixedXRotation, rotation.eulerAngles.y, rotation.eulerAngles.z);
+
+        // 카메라의 회전 값 적용
+        transform.rotation = rotation;
     }
 }
