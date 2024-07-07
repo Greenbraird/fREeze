65056b5729d72f19b5402fc5977c6559dabc67e2
Kimsijun <siwancom22@@cau.ac.kr>
2024-07-07 18:46:38 +0900

    Automated commit on 2024-07-07 18:46:35

---
+++ Assets/02. Scripts/Player/PlayerCollider.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class PlayerCollider : MonoBehaviour
+{
+     SplineAnimate _splineAnimate;
+
+    void Start()
+    {
+        _splineAnimate = GetComponent<SplineAnimate>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SplineContainer otherspline = other.gameObject.GetComponent<SplineContainer>();
+
+        _splineAnimate.Container = otherspline;
+        _splineAnimate.Play();
+
+
+    }
+
+    /**
+     void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        // 충돌한 물체의 태그 가져오기
+        string collidedObjectTag = collision.gameObject.tag;
+
+        //// 태그에 따라 다른 동작 수행
+        if (collidedObjectTag == "cube")
+        {
+            Debug.Log("장애물이랑 접촉했습니다!");
+            //충돌 시  필요한 event
+        }
+        else if(collidedObjectTag == "Star")
+        {
+            collision.gameObject.SetActive(false);
+            Debug.Log("물체와 충돌했습니다");
+            UIManager.Instance.GetCoinSliderEvent();
+            AudioManager.Instance.GetCoinSFXPlay();
+        }
+    }
+     **/
+}
